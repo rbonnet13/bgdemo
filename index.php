7ef853593aff4abbdff82d8c1add61ec825412ec
Kyle Bassett <kyle.bassett@arctiq.ca>
2016-11-07 14:30:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
         <div class="col-md-6 col-md-offset-3">
           <h2>Application Example</h2>
           <p>
-            <img src="bj-win.jpg">
+            <img src="bj-lose.jpg">
           </p>
         </div>
       </div>
